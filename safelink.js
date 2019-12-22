be0915a14c552a8348cd0721c60f5979e2dddca7
essaherlandy <44141039+essaherlandy@users.noreply.github.com>
2019-12-22 23:23:02 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="practice-environmental-qwareness-to-our.html"}
 if(x=="2"){xxx="this-is-advantages-of-using-cloud.html"}
-if(x=="3"){xxx="know-meaning-of-web-hosting.html"}
+if(x=="3"){xxx="super-penthouses-raising-bar-of-luxury.html"}
 if(x=="4"){xxx="understanding-what-is-hosting-what.html"}a_to_ve[i].href="https://safelinku.recipescookingtips.com/2019/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
