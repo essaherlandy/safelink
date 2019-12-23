039693fbd05da45a3b0366d0c920c1c73471f5d3
essaherlandy <44141039+essaherlandy@users.noreply.github.com>
2019-12-23 11:52:31 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="practice-environmental-qwareness-to-our.html"}
+if(x=="1"){xxx="practice-environmental-qwareness-to-our_20.html"}
 if(x=="2"){xxx="super-penthouses-raising-bar-of-luxury.html"}
-if(x=="3"){xxx="know-meaning-of-web-hosting.html"}
-if(x=="4"){xxx="understanding-what-is-hosting-what.html"}a_to_ve[i].href="https://minimtv.recipescookingtips.com/2019/12"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="know-meaning-of-web-hosting_24.html"}
+if(x=="4"){xxx="understanding-what-is-hosting-what_20.html"}a_to_ve[i].href="https://minimtv.recipescookingtips.com/2019/12"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
